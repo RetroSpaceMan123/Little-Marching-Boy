0322d447e477334828ec2a18a8d4d867a5d6f883
RetroSpaceMan123 <48300738+RetroSpaceMan123@users.noreply.github.com>
2020-12-15 11:54:09 -0500

    Return to Development
    Added Test Level and Animations

--- Little Marching Boy Source/Source/Player/Player.gd
+++ Little Marching Boy Source/Source/Player/Player.gd
@@ -14,6 +14,9 @@ var grounded: bool = false
 
 #Player Components
 onready var sprite = $Sprite
+onready var animate = $Sprite/AnimationPlayer
+onready var cam = $Camera2D
+onready var coytime = $"Coyote Time"
 
 func _physics_process(delta):
 	vel.x = 0
@@ -30,7 +33,18 @@ func _physics_process(delta):
 	if Input.is_action_pressed("jump") and is_on_floor():
 		vel.y -= jumpForce
 	
-	if vel.x < 0:
-		sprite.flip_h = false
 	if vel.x > 0:
+		sprite.flip_h = false
+	if vel.x < 0:
 		sprite.flip_h = true
+		
+	animation()
+	
+func animation():
+		if vel.x != 0:
+			animate.play("Walking")
+		else:
+			animate.play("Idle")
+
+func is_on_floor():
+	pass
