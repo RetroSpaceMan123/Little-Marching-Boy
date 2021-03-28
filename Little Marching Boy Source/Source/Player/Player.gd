0f65b7ce55ced807a9fca4c94a6a2b28cf5b47c6
RetroSpaceMan123 <48300738+RetroSpaceMan123@users.noreply.github.com>
2021-03-28 01:52:40 -0500

    Audio & Snow
    -Background Music and Basic SFX added
    -New Tileset
    -Snow

--- Little Marching Boy Source/Source/Player/Player.gd
+++ Little Marching Boy Source/Source/Player/Player.gd
@@ -6,7 +6,7 @@ var health: int = 3
 
 #Player Physics
 var gravity: int = 800
-var jumpForce: int = 60
+var jumpForce: int = 55
 var speed: int = 200
 
 var vel: Vector2 = Vector2()
@@ -16,33 +16,47 @@ var grounded: bool = true
 onready var sprite = $Sprite
 onready var animate = $Sprite/AnimationPlayer
 onready var cam = $Camera2D
-onready var coytime = $"Coyote Time"
+onready var sfx = $JumpSFX
+onready var presentSFX = $PresentSFX
 onready var ui = get_node("/root/Test Level/CanvasLayer/UI")
+
+#Audio Componets
+var jumpSFX: bool = true
 
 func _physics_process(delta):
 	vel.x = 0
 	
+	#Moves Player Left and Right
 	if Input.is_action_pressed("move_left"):
 		vel.x -= speed
 	if Input.is_action_pressed("move_right"):
 		vel.x += speed
 		
-	if !is_on_floor():
-		coytime.start()
-		
-		
-	if coytime.is_stopped():
-		grounded = false
+	#Checks to See if Player is Grounded
+	if is_on_floor():
+		grounded = true
+	elif !is_on_floor():
+		coyote()
 	
-	
+	#Initiates Player Movements
 	vel = move_and_slide(vel, Vector2.UP)
 	
+	#Gravity
 	vel.y += gravity * delta
 	
+	#Player Jump
+	if Input.is_action_pressed("jump"):
+		if grounded:
+			vel.y -= jumpForce
+			if jumpSFX:
+				sfx.play()
+				jumpSFX = false
+			
+	#Resets Level (Testing Purposes)
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
 	
-	if Input.is_action_pressed("jump") and grounded:
-		vel.y -= jumpForce
-	
+	#Orients Sprites Correctly
 	if vel.x > 0:
 		sprite.flip_h = false
 	if vel.x < 0:
@@ -50,12 +64,24 @@ func _physics_process(delta):
 		
 	animation()
 	
+#Animates Player
 func animation():
-		if vel.x != 0:
+		if !is_on_floor():
+			animate.play("Jump")
+		elif is_on_floor() and vel.x != 0:
 			animate.play("Walking")
 		else:
 			animate.play("Idle")
 
+#Collects Presents
 func collect_present(value):
+	presentSFX.play()
 	score += value
 	ui.set_score_text(score)
+
+#Coyote Time
+func coyote():
+	yield(get_tree().create_timer(.1),"timeout")
+	grounded = false
+	jumpSFX = true
+	
